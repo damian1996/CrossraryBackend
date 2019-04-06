88ca6d39708e25a2ea7ac6e07c03e693d7433629
Damian Stachura <damian260@onet.pl>
2019-04-06 15:42:01 +0200

    Eh

--- src/main/kotlin/Crossrary/Hello.kt
+++ src/main/kotlin/Crossrary/Hello.kt
@@ -1,6 +1,7 @@
 package Crossrary
+import io.javalin.Javalin
 
 fun main(args: Array<String>) {
-    println("Hello, World")
+    val app = Javalin.create().start(7000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
 }
-
